6dc3647cf83cb0d62a9fed0518e9fe76c5d86853
Salvador E. Tropea <salvador@inti.gob.ar>
2026-08-11 11:33:04 -0300

    [PDF] Decoupled from pcbnew and removed line_width

--- kibot/out_pdf.py
+++ kibot/out_pdf.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020-2023 Salvador E. Tropea
-# Copyright (c) 2020-2023 Instituto Nacional de Tecnología Industrial
+# Copyright (c) 2020-2026 Salvador E. Tropea
+# Copyright (c) 2020-2026 Instituto Nacional de Tecnología Industrial
 # Copyright (c) 2018 John Beard
 # License: GPL-3.0
 # Project: KiBot (formerly KiPlot)
 # Adapted from: https://github.com/johnbeard/kiplot
-from pcbnew import (PLOT_FORMAT_PDF, FromMM, ToMM)
 from .out_any_layer import AnyLayer
 from .drill_marks import DrillMarks
 from .gs import GS
@@ -20,26 +19,20 @@ class PDFOptions(DrillMarks):
     def __init__(self):
         super().__init__()
         with document:
-            self.line_width = 0.1
-            """ [0.02,2] For objects without width [mm] (KiCad 5) """
             self.mirror_plot = False
             """ Plot mirrored """
             self.negative_plot = False
             """ Invert black and white """
-        self._plot_format = PLOT_FORMAT_PDF
+        self._plot_format = GS.PLOT_FORMAT_PDF
 
     def _configure_plot_ctrl(self, po, output_dir):
         super()._configure_plot_ctrl(po, output_dir)
         po.SetMirror(self.mirror_plot)
-        if GS.ki5:
-            po.SetLineWidth(FromMM(self.line_width))
         po.SetNegative(self.negative_plot)
 
     def read_vals_from_po(self, po):
         super().read_vals_from_po(po)
         self.mirror_plot = po.GetMirror()
-        if GS.ki5:
-            self.line_width = ToMM(po.GetLineWidth())
         self.negative_plot = po.GetNegative()
 
 
