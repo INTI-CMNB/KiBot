164826b1970ed896614bc4b9d010b9bf5a360da1
Salvador E. Tropea <salvador@inti.gob.ar>
2026-08-12 07:39:21 -0300

    [Tests][Fixed] Removed test_line_width_min/max
    - This option is no longer valid
    - Was a KiCad 5 option

--- tests/test_plot/test_yaml_errors.py
+++ tests/test_plot/test_yaml_errors.py
@@ -896,24 +896,6 @@ def test_download_datasheets_no_output(test_dir):
 
 
 @pytest.mark.indep
-def test_line_width_min(test_dir):
-    """ line_width < min """
-    ctx = context.TestContext(test_dir, 'bom', 'error_wrong_line_width_min')
-    ctx.run(EXIT_BAD_CONFIG)
-    assert ctx.search_err(r"`line_width` outside its range ")
-    ctx.clean_up(keep_project=True)
-
-
-@pytest.mark.indep
-def test_line_width_max(test_dir):
-    """ line_width > max """
-    ctx = context.TestContext(test_dir, 'bom', 'error_wrong_line_width_max')
-    ctx.run(EXIT_BAD_CONFIG)
-    assert ctx.search_err(r"`line_width` outside its range ")
-    ctx.clean_up(keep_project=True)
-
-
-@pytest.mark.indep
 def test_error_empty_pth_id(test_dir):
     ctx = context.TestContext(test_dir, PRJ, 'error_empty_pth_id')
     ctx.run(EXIT_BAD_CONFIG)
